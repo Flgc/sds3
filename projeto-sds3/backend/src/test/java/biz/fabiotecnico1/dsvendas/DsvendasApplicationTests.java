42135ffe351277e0b8cd5f1d366bfd244046f54a
Fabio Luis <ml@fabiotecnico1.biz>
2021-05-03 19:54:34 -0300

    Project created - Back and Frontend

---
+++ projeto-sds3/backend/src/test/java/biz/fabiotecnico1/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.fabiotecnico1.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
