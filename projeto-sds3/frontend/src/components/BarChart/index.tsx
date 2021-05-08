b56f8e48af669c35987747798b5ab77fe3221c3b
Fabio Luis <ml@fabiotecnico1.biz>
2021-05-08 18:29:49 -0300

    DataTable integration - Implemented

--- projeto-sds3/frontend/src/components/BarChart/index.tsx
+++ projeto-sds3/frontend/src/components/BarChart/index.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSuccess } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
-import { round } from 'types/format';
+import { round } from 'utils/format';
 
 type SeriesData = {
   name: string;
--- projeto-sds3/frontend/src/types/format.ts
+++
@@ -1,4 +0,0 @@
-export const round = (value: number, precision: number) => {
-  var multiplier = Math.pow(10, precision || 0);
-  return Math.round(value * multiplier) / multiplier;
-};
